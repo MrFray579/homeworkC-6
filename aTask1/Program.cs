4a21ebf7b6db44bdbf73a1a8af9be14d3470f9a5
Fray <kozar20000@mail.ru>
2022-07-17 23:25:02 +0300

    numbers > 0

---
+++ aTask1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите количество проверяемых чисел:");
+int numbers = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+for (int i = 0; i < numbers; i++)
+{
+    int digit = Convert.ToInt32(Console.ReadLine());
+    if (digit > 0)
+    count++;
+}
+
+Console.Write("Positives {0}", count);
+Console.WriteLine();
