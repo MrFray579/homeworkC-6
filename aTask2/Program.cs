e4dc6da5de3dd6a869e2295986d04ca164634acf
Fray <kozar20000@mail.ru>
2022-07-17 23:36:21 +0300

    Coordinates

---
+++ aTask2/Program.cs
@@ -0,0 +1,10 @@
+﻿void Coordinates (double k1, double k2, double b1, double b2)
+{
+    double x = (b1 - b2) / (k2 - k1);
+    double y = x * k1 + b1; 
+    Console.WriteLine("Coordinates {0}, {1}", x, y);
+}
+
+Coordinates(2, 5, 7, 10);
+Coordinates(9, 6, 8, 1);
+Coordinates(1, 7, 3, 15);
